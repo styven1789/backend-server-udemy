5b3562535d6c70d4dd44a8753141e4dc5f38e183
Estivenson Carvajal Salazar <ecarvajal@ingeneo.local>
2020-03-06 10:33:45 -0500

    =Primer commit

---
+++ app.js
@@ -0,0 +1,29 @@
+// Requires
+var express = require('express');
+var mongoose = require('mongoose');
+
+// Inicializar variables
+var app = express();
+
+// Conexion a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (error, res) => {
+
+    if (error) throw error;
+
+    console.log('Base de datos online');
+
+});
+
+// Rutas
+app.get('/', (req, res, next) => {
+
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Petición realizada correctamente'
+    });
+});
+
+// Escuchar peticiones
+app.listen(3000, () => {
+    console.log('Ecpress server puesto 3000 online')
+});
